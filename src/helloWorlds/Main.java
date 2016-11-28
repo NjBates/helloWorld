26c0abc71647447b637ca42384c10dfa1d6cd0c2
Turner-Tobie <turner.tobie@yahoo.com>
2016-11-28 15:13:42 -0500

    Rename Main

---
+++ src/helloWorlds/Main.java
@@ -0,0 +1,12 @@
+package helloWorlds;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Helloo World");
+		System.out.println("Hello Cruel World");
+
+	}
+
+}
--- src/helloWorlds/helloWorld.java
+++
@@ -1,9 +0,0 @@
-package helloWorlds;
-
-public class helloWorld{
-public static void Main (String args[]){
-
-	System.out.println("Helloo World");
-	System.out.println("Hello Cruel World");
-}
-}
